68326cf16b718c13fd6fa61556b7828c7cda041e
Artur Falborski <falborski.artur@gmail.com>
2018-11-23 19:58:37 +0100

    add top bar wrapper

---
+++ src/common/TopBar/TopBar.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import { TopBarWrapper} from './TopBar.styles';
+
+export default class TopBar extends Component {
+  render() {
+    return (
+      <TopBarWrapper>
+      </TopBarWrapper>
+    )
+  }
+}
---
+++ src/common/TopBar/TopBar.styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+const TopBarWrapper = styled.div`
+  background: ${({ theme }) => theme.mainColor };
+  padding: 1.5%;
+`;
+
+export { TopBarWrapper };
