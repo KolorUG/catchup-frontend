f53b0212522f289a4dace999465f7c7b80aa9776
Artur Falborski <falborski.artur@gmail.com>
2018-11-23 19:55:55 +0100

    create initial themes

---
+++ src/common/themes.js
@@ -0,0 +1,12 @@
+const themes = {
+  grayscale: {
+    mainColor: '#666666',
+    subColor: '#AAAAAA'
+  },
+  blackAndYellow: {
+    mainColor: '#000000',
+    subColor: 'yellow'
+  }
+};
+
+export { themes };
