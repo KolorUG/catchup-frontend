83dd1e09a6575a9f6bd9da211b8fd8e52f71bf82
Artur Falborski <falborski.artur@gmail.com>
2018-11-24 20:01:25 +0100

    add font weight theme

--- src/common/themes.js
+++ src/common/themes.js
@@ -6,6 +6,8 @@ const themes = {
   main: {
     white: "#ffffff",
     fontColor: '#b8dff0',
+    fontThin: '100',
+    fontRegular: '400',
     borderColor: '#b8dff0',
     submittingColor: 'rgba(184, 223, 240, 0.33)',
     errorColor: '#ffb487'
